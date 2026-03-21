c8c379162e529294207610acfec487d52dd39b62
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-21 19:08:47 -0300

    feat: add savement of enrollment into views

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from .forms import EnrollmentForm
 
 def home(request):
     return HttpResponse("Adicione aqui a página principal!")
 
-def new_enrollment(request):
-    return HttpResponse("Adicione aqui o formulário de inscrição!")
+def new_enrollment(request:HttpRequest):
+    if request.method == "POST":
+        form = EnrollmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("enrollments:enrollment_received")
+
+    context = {
+        "form": EnrollmentForm
+    }
+    return render(request, 'enrollments/new_enrollment.html', context)
 
 def enrollment_received(request):
     return HttpResponse("Adicione aqui a página Inscrição Recebida!")
