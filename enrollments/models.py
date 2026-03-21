6ac2c79a6078bc5f0465906736c0d24df530085b
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-21 08:52:10 -0300

    feat: Enrollment model created

--- enrollments/models.py
+++ enrollments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Enrollment(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    church = models.CharField(max_length=100)
+    celphone = models.CharField(max_length=11)
+    emergency_contact = models.CharField(max_length=11)
+    email = models.EmailField()
+    local_proof = models.CharField()
+    payment_status = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    consent_given = models.CharField()
+    ip_address = models.CharField()
