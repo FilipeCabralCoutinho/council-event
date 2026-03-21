8dfd2f152b3505a5dfcf133bfd1d179f66820214
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-21 08:27:45 -0300

    feat: add urls routes

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home():
+def home(request):
     return HttpResponse("Adicione aqui a página principal!")
 
-def new_enrollment():
+def new_enrollment(request):
     return HttpResponse("Adicione aqui o formulário de inscrição!")
 
-def enrollment_received():
+def enrollment_received(request):
     return HttpResponse("Adicione aqui a página Inscrição Recebida!")
