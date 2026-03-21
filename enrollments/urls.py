7b0f423990dd236662f17d6e953b2a2d07d00cf4
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-21 19:07:07 -0300

    feat: add templates files, and name in routes

--- enrollments/urls.py
+++ enrollments/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "enrollments"
+
 urlpatterns = [
-    path("", views.home),
-    path("new-enrollment/", views.new_enrollment),
-    path("enrollment-received/", views.enrollment_received)
+    path("", views.home, name='home'),
+    path("new-enrollment/", views.new_enrollment, name='new_enrollment'),
+    path("enrollment-received/", views.enrollment_received, name='enrollment_received')
 ]
