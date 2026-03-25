db1390ae1bc9b07ddb535b71e69f62b911303f60
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-25 20:57:37 -0300

    chore: optional and default field settings in Enrollment Model

--- enrollments/models.py
+++ enrollments/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Enrollment(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
+    id = models.IntegerField(primary_key=True, auto_created=True, unique=True)
     name = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11)
     church = models.CharField(max_length=100)
     celphone = models.CharField(max_length=11)
     emergency_contact = models.CharField(max_length=11)
     email = models.EmailField()
-    local_proof = models.CharField()
-    payment_status = models.CharField()
+    local_proof = models.CharField(blank=True)
+    payment_status = models.CharField(default="PENDENTE")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     consent_given = models.BooleanField(default=False)
-    ip_address = models.CharField()
+    ip_address = models.CharField(blank=True)
 
     def __str__(self):
         return f"{self.id} | Nome: {self.name} | CPF: {self.cpf} | Status Pagamento: {self.payment_status} | Local Comprovante: {self.local_proof}"
