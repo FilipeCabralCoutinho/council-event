12df5bd8dbcac224749e975d3c05f138453998f8
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-04-10 18:47:02 -0300

    feat: Creating a messages.py file to centralize the text of messages that will be sent by email.

---
+++ enrollments/messages.py
@@ -0,0 +1,78 @@
+def text_fallback_new(enrollment):
+    return f"""
+        Confirmação de inscrição – IX Concílio da 6ª Região
+
+
+        Graça e paz!
+
+        Recebemos com alegria a sua inscrição no IX Concílio Regional.
+        Será um tempo especial de comunhão, crescimento e direcionamento
+        de Deus para todos nós. Para concluir o seu processo de participação,
+        pedimos que envie o comprovante de pagamento para este mesmo e-mail.
+        Assim que o pagamento for confirmado no total, você receberá o seu
+        voucher de acesso ao Acampamento Efraim, que garantirá sua entrada
+        no evento.
+        
+        Ficamos felizes por ter você conosco nesse momento tão importante!
+        
+        Qualquer dúvida, estamos à disposição.
+        
+        Deus abençoe!
+
+        Atenciosamente,
+        Organização do IX Concílio Regional
+
+        ==============================
+        DADOS DA INSCRIÇÃO
+        ==============================
+
+        NR INSCR: {enrollment.id}
+        NOME: {enrollment.nome}
+        DISTRITO: {enrollment.distrito}
+        IGREJA: {enrollment.igreja}
+        CELULAR: {enrollment.whatsapp}
+        FUNÇÃO: {enrollment.funcao}
+        APTO CONCÍLIO GERAL?: {enrollment.apto_concilio}
+        POSSUI COMORBIDADE?: {enrollment.possui_comorbidade}
+        QUAL COMORBIDADE?: {enrollment.qual_comorbidade}
+        PERNOITE?: {enrollment.pernoite}
+        STATUS PAGAMENTO: {enrollment.status_pagamento}
+
+        ==============================
+        INFORMAÇÕES DO EVENTO
+        ==============================
+
+        Data:
+        26, 27 e 28 de novembro de 2026
+
+        Local:
+        Acampamento Efraim
+
+        ==============================
+        DÚVIDAS
+        ==============================
+
+        Em caso de dúvidas, entre em contato com a organização.
+
+        Igreja Metodista Wesleyana
+        6ª Região
+        """
+
+def start_text_new():
+    return (
+            """
+            Graça e paz!
+            Recebemos com alegria a sua inscrição no IX Concílio Regional. Será um tempo especial de comunhão, crescimento e direcionamento de Deus para todos nós.
+            Para concluir o seu processo de participação, pedimos que envie o comprovante de pagamento para este mesmo e-mail.
+            Assim que o pagamento for confirmado no total, você receberá o seu voucher de acesso ao Acampamento Efraim, que garantirá sua entrada no evento.
+
+            Ficamos felizes por ter você conosco nesse momento tão importante!
+
+            Qualquer dúvida, estamos à disposição.
+
+            Deus abençoe!
+
+            Atenciosamente,
+            Organização do IX Concílio Regional
+            """
+        )
