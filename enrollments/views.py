2ae03e8e8539f5050eb7435534ba6131ec91c530
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-04-10 18:49:25 -0300

    feat: add views and modify new_enrollment view

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpRequest
-from .forms import EnrollmentForm
+from django.contrib import messages
+from django.http import HttpResponse, HttpRequest, JsonResponse, FileResponse
+from .forms import InscricaoForm
+from .models import Igreja, Inscricoes
+from .services import Services
+
+service = Services()
 
 def home(request):
     return HttpResponse("Página principal!")
 
 def new_enrollment(request:HttpRequest):
     if request.method == "POST":
-        form = EnrollmentForm(request.POST)
+        form = InscricaoForm(request.POST)
+        cpf = request.POST.get("cpf")
+
         if form.is_valid():
             form.save()
+            enrollment = Inscricoes.objects.get(cpf=cpf)
+
+            service.payment(enrollment.id)
+
+            service.send_email(enrollment)
+
             return redirect("enrollments:enrollment_received")
+        else:
+            if Inscricoes.objects.filter(cpf=cpf).exists():
+                messages.error(request, "CPF JÁ CADASTRADO!")
+                return redirect('enrollments:new_enrollment')
+
 
     context = {
-        "form": EnrollmentForm
+        "form": InscricaoForm
     }
     return render(request, 'enrollments/new_enrollment.html', context)
 
 def enrollment_received(request):
     return HttpResponse("Página Inscrição Recebida!")
+
+def busca_igrejas_por_distrito(request):
+    distrito_id = request.GET.get('distrito_id')
+    # Filtra as igrejas baseadas no ID do distrito recebido
+    igrejas = Igreja.objects.filter(distrito_id=distrito_id).values('id', 'nome').order_by('nome')
+    return JsonResponse(list(igrejas), safe=False)
+
+def export_file(request, file):
+    return FileResponse(file, as_attachment=True, filename="Inscrições")
