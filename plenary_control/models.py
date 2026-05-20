d1d6d1228ae377b17860aa9ca5483b491c2635ed
Filipe Coutinho <filipeccoutinho2017@gmail.com>
2026-05-20 07:39:41 -0300

    feat: define Participante model with necessary fields

--- plenary_control/models.py
+++ plenary_control/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Participante(models.Model):
+    id_participante = models.AutoField(primary_key=True)
+    id_inscricao = models.IntegerField(blank=False, null=False)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    whatsapp = models.CharField(max_length=20)
+    cpf = models.CharField(max_length=14, unique=True)
+    status_pagamento = models.CharField(max_length=20)
+    distrito = models.CharField(max_length=255)
+    igreja = models.CharField(max_length=255)
+    funcao = models.CharField(max_length=50)
+    pernoite = models.CharField(max_length=3)
+    comorbidade = models.CharField(max_length=3)
+    qual_comorbidade = models.CharField(max_length=255, blank=True)
+    no_plenario = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Nr_Participante: {self.id_participante} | Nome: {self.nome} | Distrito: {self.distrito} | Igreja: {self.igreja} | No Plenário: {self.no_plenario}"
