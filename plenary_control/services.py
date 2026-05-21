d9549925da1add0620e19801ff1e9d65bf0d5285
Filipe Coutinho <filipeccoutinho2017@gmail.com>
2026-05-21 07:16:25 -0300

    feat: implement PlenaryControlService with methods to retrieve confirmed enrollments and create new participants

--- plenary_control/services.py
+++ plenary_control/services.py
@@ -1,4 +1,35 @@
 from ..enrollments.models import Inscricoes
+from .models import Participantes
 
-def get_all_confirmed_enrollments():
-    return Inscricoes.objects.filter(status_pagamento='CONFIRMADO')
+
+class PlenaryControlService:
+    def get_all_confirmed_enrollments(self):
+        return Inscricoes.objects.filter(status_pagamento='CONFIRMADO')
+
+    def create_new_participants(self):
+        confirmed_participants = self.get_all_confirmed_enrollments()
+
+        for participant in confirmed_participants:
+            id_enrollment = participant.id_inscricao
+            check_participant = Participantes.objects.filter(
+                id_inscricao=id_enrollment
+            ).first()
+
+            if not check_participant:
+                new_participant = Participantes(
+                    id_inscricao=id_enrollment,
+                    nome=participant.nome,
+                    email=participant.email,
+                    whatsapp=participant.whatsapp,
+                    cpf=participant.cpf,
+                    status_pagamento=participant.status_pagamento,
+                    distrito=participant.distrito,
+                    igreja=participant.igreja,
+                    funcao=participant.funcao,
+                    pernoite=participant.pernoite,
+                    comorbidade=participant.comorbidade,
+                    qual_comorbidade=participant.qual_comorbidade
+                )
+                new_participant.save()
+
+            return True
